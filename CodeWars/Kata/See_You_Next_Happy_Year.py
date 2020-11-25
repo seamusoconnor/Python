c261294cef4b6f9c7a4a98d4f5861a17a9ea046b
Seamus O'Connor <oseamus@amazon.com>
2020-11-25 00:44:50 +0000

    add linting and docstring

--- CodeWars/Kata/See_You_Next_Happy_Year.py
+++ CodeWars/Kata/See_You_Next_Happy_Year.py
@@ -1,8 +1,18 @@
 #!/usr/local/bin/python3
 
-def next_happy_year(year):
 
-    year+=1
+def next_happy_year(year) -> int:
+    """
+    A function that takes a positive integer between 1000 and 9000
+    and returns the next 'Heppy Year' (no duplicate numbers)
+
+    Parameters:
+    year : int
+
+    Returns:
+    year : int
+    """
+    year += 1
     while 1000 <= year <= 9999:
         int_list = [int(i) for i in str(year)]
         se = set(int_list)
@@ -10,9 +20,7 @@ def next_happy_year(year):
             return year
 
         else:
-            year+=1
-
+            year += 1
 
 
 print(next_happy_year(1001))
-
